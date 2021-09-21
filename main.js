24f760762d11f1c6277f005aea9642b9fc93ad3c
basic-coding <82214035+basic-coding@users.noreply.github.com>
2021-09-21 10:58:54 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -7,3 +7,17 @@ function addUser(){
 
     window.location="math.html";
 }
+
+function send(){
+    number1=document.getElementById("word").value;
+    number2=document.getElementById("word1").value;
+    actual_number=parseInt(number1)*parseInt(number2);
+
+    question_number="<h4>"+ number1 + "X" +number2+"</h4>";
+    input= "<br>Answer: <input type='text' id='input_check_box'>";
+    check_button = "<br><br><button class='btn btn-primary' onclick='check()'>Check</button>";
+    row=question_number+input+actual_number;
+    document.getElementById("output").innerHTML=row;
+    document.getElementById("word").value="";
+    document.getElementById("word1").value="";
+}
